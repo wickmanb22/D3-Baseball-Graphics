0355ec8d5769705b7fdb8f3a020e358d9b6132e9
wickmanb22 <66176288+wickmanb22@users.noreply.github.com>
2020-05-30 15:00:04 -0400

    Updated with explanation

--- Player Stats/Making Player Stat Databases.R
+++ Player Stats/Making Player Stat Databases.R
@@ -6,26 +6,17 @@ library(baseballr)
 library(tidyverse)
 library(rvest)
 
-#Add additional year
-  D32020IDs <- read.csv("~/R/Baseball Analysis/Intro Baseball Analysis/D3 Player Stats/IDs2020D3.csv")
-  D32020IDs$year = 2018
-  #Create 2018 IDs (Drop St. Elizabeth because new program as of 2020)
-  D318IDs <- D32020IDs[-c(387),]
-  #Merge with D3IDs, will need those for visualizations later
-    D3IDs1920 <- read.csv("~/R/Baseball Analysis/Intro Baseball Analysis/FullD3Rankings/D3BG2/DataPrep/D3IDs1920.csv")
-    D3IDs1920$X <- NULL
-  D3IDs181920 <- rbind(D318IDs, D3IDs1920)
-  #write.csv(D3IDs181920, "D3IDs890.csv", row.names = FALSE)
-  
+#Load in D3IDs1920 (in same folder as this file 
+
 #Changed to do the 2019 batting season
 #get list of hitting and pitching data by team
 Batting <- list()
 Pitching <- list()
-for (i in 1:nrow(D318IDs)) {
+for (i in 1:nrow(D3IDs1920)) {
   #tryCatch function overwrites the error of no data availabe (if any) to go to the next school IDs
   tryCatch({
-    Batting[[i]] <- ncaa_scrape(D318IDs$school_id[i], year = D318IDs$year[i], type = "batting")
-    Pitching[[i]] <- ncaa_scrape(D318IDs$school_id[i], year = D318IDs$year[i], type = "pitching")
+    Batting[[i]] <- ncaa_scrape(D3IDs1920$school_id[i], year = D3IDs1920$year[i], type = "batting")
+    Pitching[[i]] <- ncaa_scrape(D3IDs1920$school_id[i], year = D3IDs1920$year[i], type = "pitching")
   },error=function(e){})
 }
 
@@ -56,6 +47,6 @@ D3Bat$OPS = D3Bat$OBPct+D3Bat$SlgPct
 D3Bat1 <- rbind(D3Bat, D3BatAll)
 D3Pitch1 <- rbind(D3Pitch, D3PitchAll)
 
-#Put into a csv file so don't have to keep running code
-write.csv(D3Bat1,"D3Bat18.csv", row.names = F)
-write.csv(D3Pitch1,"D3Pitch18.csv", row.names = F)
+#Save as csv files that will be used in the subsequent cleaning R scripts
+#write.csv(D3Bat1,"D3Bat18.csv", row.names = F)
+#write.csv(D3Pitch1,"D3Pitch18.csv", row.names = F)
