f944ef153fb696343aef3db6c9fe65d0e6af3875
bwickman~ <bwickman5@gmail.com~>
2022-10-24 08:05:39 -0500

    Update rankings with RPI and SOS

--- Rankings/D3Rankings_v2_p2.R
+++ Rankings/D3Rankings_v2_p2.R
@@ -6,6 +6,8 @@
   library(tidyverse)
   library(baseballr)
   library(stringr)
+  library(data.table)
+  library(ggplot2)
 
 # Load workspace from part 1
   load("~/GitProjects/D3-Baseball-Graphics/Rankings/D3Rankings_v2_p1_Workspace.RData")
@@ -67,21 +69,58 @@
         group_by(opponent) %>% 
         summarise(number = n()) %>%
         mutate(school_id = D3IDs4$school_id[i])
-      names(SOSList)[i] <- D3IDs4$School[i]}
+      colnames(SOSList[[i]])[1] <- "School"
+      names(SOSList)[i] <- D3IDs4$School[i]
+      }
   
   # Move to dataframe
     SOSdf <- rbindlist(SOSList, idcol = TRUE)
-    colnames(SOSdf)[1] <- "school"
+    colnames(SOSdf)[1] <- "School"
   
   # Win Loss dataframe
-    wl_df <- D3IDs4 %>% select(opponent = 1,7:9)
+    wl_df <- D3IDs4 %>% select(School = 1,7:9)
     wl_df[,2:4] <- lapply(wl_df[,2:4],as.numeric) # Convert W,L,T columns from character to numeric
-    wl_df$wpct <- round((wl_df$W + 0.5*wl_df$T)/(wl_df$W + wl_df$L + wl_df$T),3)
-    wl_df2 <- wl_df[,c(1,5)]
+
+  # Calculate opponents WL%
+    SOS_OppWL <- list()
+    for (i in 1:length(SOSList)){
+      df1 <- merge(SOSList[[i]], wl_df, by = "School", all.x = TRUE) %>% drop_na()
+      df2 <- df1 %>% mutate(W = sum(number * W), L = sum(number * L), T = sum(number * T))
+      SOS_OppWL[[i]] <- df2[1,4:6]
+      names(SOS_OppWL)[i] <- wl_df$School[i]
+    }
     
-  # Calculate opponents WL%, match uchicago
-    setdiff(SOSdf$opponent, wl_df2$opponent) # jeez
-    oppDF <- merge(SOSdf,wl_df2, all.x = TRUE, by = "opponent")
+    # Bind list to dataframe
+      team_opp_wl <- rbindlist(SOS_OppWL, idcol = "School") %>%
+        mutate(opp_win_pct = (2*(W+T))/(2*(W+L+T))*100)
+      
+  # Calculate Schools' Schools' WL%
+    SOS_OppOppWL <- list()
+    for (i in 1:length(SOSList)){
+        df1 <- merge(SOSList[[i]], team_opp_wl[,-5], by = "School", all.x = TRUE) %>% drop_na()
+        df2 <- df1 %>% mutate(W = sum(number * W), L = sum(number * L), T = sum(number * T))
+        SOS_OppOppWL[[i]] <- df2[1,4:6]
+        names(SOS_OppOppWL)[i] <- team_opp_wl$School[i]
+    }
     
-  # Group by school, calculate number * winpct
-    
+    # Bind list to dataframe and do calculations
+      team_oppopp_wl <- rbindlist(SOS_OppOppWL, idcol = "School") %>% 
+        mutate(opp_opp_win_pct = (2*(W+T))/(2*(W+L+T))*100)
+  
+  # Bring relevant data back to one dataframe
+    # Convert school column to factor
+      D3IDs4$School <- as.factor(D3IDs4$School)
+      winpct_calc$School <- as.factor(winpct_calc$School)
+      winpct_calc <- merge(team_opp_wl[,c(1,5)],team_oppopp_wl[,c(1,5)], by = "School")
+      D3IDs5 <- merge(D3IDs4, winpct_calc, by = "School")
+      
+    # Strength of schedule calculation
+      D3IDs5[,7:9] <- lapply(D3IDs5[,7:9],as.numeric) # Convert W,L,T columns from character to numeric
+      d3_team <- D3IDs5 %>% mutate(win_pct = round((W+0.5*T)/(W+L+T),3),
+                                   SOS = round((2*opp_win_pct + opp_opp_win_pct)/3,2),
+                                   RPI = round(((0.25 * win_pct) + (0.5*opp_win_pct) + (0.25*opp_opp_win_pct)),2)) %>% arrange(desc(win_pct))
+
+  # Select relevant columns
+      d3_team_clean <- d3_team[,c(1,2,3,8,7,9,12,13,14)] 
+      colnames(d3_team_clean)[7] <- "WinPct"
+      #write.csv(d3_team_clean, "Team_Records.csv", row.names=FALSE)
